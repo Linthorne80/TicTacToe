e433496a42f51add87e85b802201aefc6243bd6e
brookelynn upshall <124837049+Linthorne80@users.noreply.github.com>
2023-09-07 13:17:27 -0230

    Add files via upload

---
+++ TicTacToe.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        char[][] board = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+        char currentPlayer = 'X';
+        boolean gameEnded = false;
+
+        while (!gameEnded) {
+            // Print the board
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    System.out.print(board[i][j]);
+                    if (j < 2) {
+                        System.out.print(" | ");
+                    }
+                }
+                System.out.println();
+                if (i < 2) {
+                    System.out.println("---------");
+                }
+            }
+
+            // Get the user's move
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter row (0-2): ");
+            int row = scanner.nextInt();
+            System.out.print("Enter column (0-2): ");
+            int col = scanner.nextInt();
+
+            // Make the move
+            if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ') {
+                board[row][col] = currentPlayer;
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            } else {
+                System.out.println("Invalid move! Try again.");
+            }
+
+            // Check if the game has ended
+            if (checkWin(board, currentPlayer)) {
+                gameEnded = true;
+                System.out.println("Player " + currentPlayer + " has won!");
+                continue;
+            }
+
+            // Check if the game is a draw
+            if (isDraw(board)) {
+                gameEnded = true;
+                System.out.println("The game is a draw!");
+                continue;
+            }
+
+            // Swap players
+
+            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+        }
+    }
+
+    public static boolean checkWin(char[][] board, char player) {
+        // Check rows
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
+                return true;
+            }
+        }
+
+        // Check columns
+        for (int j = 0; j < 3; j++) {
+            if (board[0][j] == player && board[1][j] == player && board[2][j] == player) {
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+            return true;
+        }
+        if (board[2][0] == player && board[1][1] == player && board[0][2] == player) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean isDraw(char[][] board) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
